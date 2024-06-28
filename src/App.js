224060212a8f4dd7777ef09d09550b9b1dd71efb
KHalil Touil <touikhalil2004s@gmail.com>
2024-06-28 14:02:35 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    lll
+      </header>
+    </div>
+  );
+}
+//instalation ta3 roboto 
+//fl app ykon browserrouter fih  nav wl routes 
+//3 router ta3 var
+//comp folder fih nav feed videodetailles channelid search 
+export default App;
